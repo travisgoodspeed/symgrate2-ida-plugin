067cea83bca94d8739f25984b90eaf1a4b1ddd1d
evm <evm.ftw@gmail.com>
2021-09-14 00:47:11 -0400

    Working 6.x version

--- Symgrate2Query_6x.py
+++ Symgrate2Query_6x.py
@@ -10,33 +10,28 @@
 ## database, in order to recognize standard functions from a variety
 ## of embedded ARM development kits.
 
-import httplib;
+import Symgrate2 
 import idc
 
-#Must match the server.
-LEN=18
+def ida_renamefunctions(j):
+    x=json.loads(j)
+    for f in x:
+        fnameu=x[f]["Name"]
+        fname=fnameu.encode('utf-8')
+        print("renaming %s to %s" % (f,fname))
+        fadr=int(f,16)
+        idc.MakeName(fadr,fname)
+       
 
-def queryfns(conn, q):
-    """Queries the server for a dozen or more functions."""
-    conn.request("GET", "/fns?"+q) 
-    r1 = conn.getresponse()
-    # print r1.status, r1.reason
-    # 200 OK ?
-    toret=None;
-    if r1.status==200:
-        data = r1.read();
-        if len(data)>2:
-            print data.strip();
-
-    return toret;
-
-#fncount=currentProgram.getFunctionManager().getFunctionCount();
-#monitor.initialize(fncount);
+def ida_functionprefix(fun):
+    """Returns the first eighteen bytes of a function as ASCII."""
+    B=bytearray(idc.GetManyBytes(adr, Symgrate2.SEARCHLEN));
+    bstr="";
+    for b in B: bstr+="%02x"%(0x00FF&b)
+    return bstr
 
 # Iterate over all the functions, querying from the database and printing them.
 fnhandled=0;
-
-conn = httplib.HTTPConnection("symgrate.com",80)
 
 qstr="";
 
@@ -57,22 +52,26 @@ while (f != idc.BADADDR) and (f <= end):
     adrstr="%x"%f;
     res=None;
 
-    B=bytearray(idc.GetManyBytes(adr, LEN));
-    bstr="";
-    for b in B: bstr+="%02x"%(0x00FF&b)
-
+    bstr = ida_functionprefix(f)
     # We query the server in batches of 64 functions to reduce HTTP overhead.
     qstr+="%s=%s&"%(adrstr,bstr)
     f = idc.NextFunction(f)
 
     if fnhandled&0x3F==0 or f is None:
-        res=queryfns(conn,qstr);
-        #monitor.setProgress(fnhandled);
+        res=Symgrate2.queryjfns(qstr);
         qstr="";
+        if res!=None:
+            Symgrate2.jprint(res)
+            #optionally rename functions to the values found in the query
+            ida_renamefunctions(res)
     
     fnhandled+=1;
 
-conn.close();
+res=Symgrate2.queryjfns(qstr)
+if res!=None: 
+    Symgrate2.jprint(res)
+    #optionally rename functions
+    ida_renamefunctions(res)
 
 
 #Queries an example sprintf for testing.
